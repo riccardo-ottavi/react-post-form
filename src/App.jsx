71ba3964a0ee8401239972a5e71cf044d003a11a
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-03 15:45:03 +0200

    inserisci i primi input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: ""
+  })
+
+  function handleFormData(e){
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <>
-      
+      <form action="">
+        <input type="text"
+          name='author'
+          value={formData.author}
+          onChange={handleFormData}
+          placeholder="inserisci il nome dell'autore"
+        />
+        <input type="text"
+          name='tile'
+          value={formData.title}
+          onChange={handleFormData}
+          placeholder='Inserisci il titolo del post'
+        />
+        <input type="text"
+          name='body'
+          value={formData.body}
+          onChange={handleFormData}
+          placeholder='Inserisci il contenuto del post'
+        />
+
+        
+      </form>
     </>
   )
 }
