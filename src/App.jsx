9b2897502ccc037d0e64671e3d29020934f3882a
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-03 16:34:33 +0200

    inizializza un componente per il titolo della pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import NewArticleForm from './assets/components/NewArticleForm'
+import MyHeader from './assets/components/MYHeader'
 
 function App() {
   return (
     <>
+      <MyHeader/>
       <NewArticleForm/>
     </>
   )
---
+++ src/assets/components/MYHeader.jsx
@@ -0,0 +1,9 @@
+export default function MyHeader() {
+    return(
+        <header>
+            <div className="page-title">
+                <h1>Aggiungi Articolo</h1>
+            </div>
+        </header>
+    )
+}
