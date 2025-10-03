087525b6b38ed9312fc5348e0f328dfce05c6a93
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-03 16:32:20 +0200

    crea un componente per il form

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,11 @@
-
-import { useState } from 'react'
 import './App.css'
+import NewArticleForm from './assets/components/NewArticleForm'
 
 function App() {
-  const Endpoint = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
-  const [formData, setFormData] = useState({
-    author: "",
-    title: "",
-    body: "",
-    public: null
-  })
-
-  function handleFormData(e){
-    console.log(formData)
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  function handleSubmit(e){
-    e.preventDefault();
-    axios.post(Endpoint, formData)
-    .then(res => console.log("dati inviati", res.data))
-    .catch(error => console.log(error))
-  }
-
-
-
   return (
     <>
-      <form action="" onSubmit={handleSubmit}>
-        <input type="text"
-          name='author'
-          value={formData.author}
-          onChange={handleFormData}
-          placeholder="inserisci il nome dell'autore"
-        />
-        <input type="text"
-          name='title'
-          value={formData.title}
-          onChange={handleFormData}
-          placeholder='Inserisci il titolo del post'
-        />
-        <input type="text"
-          name='body'
-          value={formData.body}
-          onChange={handleFormData}
-          placeholder='Inserisci il contenuto del post'
-        />
-
-        <select name="public" id="public" onChange={handleFormData}>
-          <option value="true">Pubblico</option>
-          <option value="false">Bozza</option>
-        </select>
-        <button type="submit">Invia Form</button>
-      </form>
+      <NewArticleForm/>
     </>
   )
 }
-
 export default App
